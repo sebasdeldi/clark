ade3f345052441e1d89638f552e612356a9ca476
Sebastian Delgado <sebasdeldi123@gmail.com>
2017-09-10 09:38:21 -0700

    pg settings

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '~> 5.1'
 
-gem 'sqlite3'
+gem 'pg'
 gem 'redis'
 gem 'puma'
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20150711103112) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer "message_id"
-    t.integer "user_id"
+    t.bigint "message_id"
+    t.bigint "user_id"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150711103112) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
@@ -37,4 +40,7 @@ ActiveRecord::Schema.define(version: 20150711103112) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "messages"
+  add_foreign_key "comments", "users"
+  add_foreign_key "messages", "users"
 end
