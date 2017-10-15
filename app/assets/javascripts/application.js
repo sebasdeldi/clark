ee5a9515f45d87995b008b3c834c900f542f5852
Sebastian Delgado <sebasdeldi@hotmail.com>
2017-10-15 01:10:30 -0500

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,41 @@
 //= require turbolinks
 //= require channels
 //= require_tree .
+
+
+var $messages = $('.messages-content'),
+d, h, m,
+i = 0;
+
+$(window).load(function() {
+$messages.mCustomScrollbar();
+setTimeout(function() {
+fakeMessage();
+}, 100);
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+  scrollInertia: 10,
+  timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
+}
+
+
+
+setTimeout(function() {
+$('.message.loading').remove();
+$('<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+setDate();
+updateScrollbar();
+i++;
+}, 1000 + (Math.random() * 20) * 100);
+
