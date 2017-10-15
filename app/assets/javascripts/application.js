f81903ed91a1bdd4e3b72939735783495f3ab1ab
Sebastian Delgado <sebasdeldi@hotmail.com>
2017-10-15 02:21:40 -0500

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,24 +15,11 @@
 //= require turbolinks
 //= require channels
 //= require_tree .
-//= require jquery.mCustomScrollbar.concat.min
 
 console.log("ywaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 var $messages = $('.messages-content'),
 d, h, m,
 i = 0;
-
-$(window).load(function() {
-  $messages.mCustomScrollbar();
-});
-
-function updateScrollbar() {
-  console.log("aaa")
-  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
-  scrollInertia: 10,
-  timeout: 0
-  });
-}
 
 function setDate(){
   d = new Date()
