bfbc6fe4041e9e1e24168f9339f5580de38ff875
Sebastian Delgado <sebasdeldi@hotmail.com>
2017-10-17 09:35:27 -0500

    setting new conv

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,14 +9,14 @@ class CommentsController < ApplicationController
     @comment = Comment.create! content: params[:comment][:content], message: @message, user: @current_user
     uri = URI.parse("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/36ce73dc-4d03-4842-957c-eb92e8ef5fbf/message?version=2017-05-26")
     request = Net::HTTP::Post.new(uri)
-    request.basic_auth("71be0b3b-ee0d-4064-b1d0-9612f8273419", "3Sk83Ixqcwvb")
+    request.basic_auth("e896f8e0-e3ef-4bab-8dd0-1caf4cafcf90", "kPhMb4vgk0Iy")
     request.content_type = "application/json"
     request.body = JSON.dump({
       "input" => {
         "text" => params[:comment][:content]
       },
       "context" => {
-        "conversation_id" => "1b7b67c0-90ed-45dc-8508-9488bc483d5b",
+        "conversation_id" => "dfdfbe3d-70c9-4600-be01-ecfcfdb337d7",
         "system" => {
           "dialog_stack" => [
             {
@@ -38,7 +38,7 @@ class CommentsController < ApplicationController
     end
 
     Comment.create! content: JSON.parse(response.body).to_h['output']['text'], message: @message, user: User.last
-  
+
   end
 
   private
