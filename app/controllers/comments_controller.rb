f8f53ca5fd8f0d77159f6d809b783335992ea428
Sebastian Delgado <sebasdeldi@hotmail.com>
2017-11-03 20:42:01 -0500

    k

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,36 +6,27 @@ class CommentsController < ApplicationController
 
 
   def create
-    puts "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"
     @comment = Comment.create! content: params[:comment][:content], message: @message, user: @current_user
     uri = URI.parse("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/dfdfbe3d-70c9-4600-be01-ecfcfdb337d7/message?version=2017-05-26")
     request = Net::HTTP::Post.new(uri)
     request.basic_auth("e896f8e0-e3ef-4bab-8dd0-1caf4cafcf90", "kPhMb4vgk0Iy")
     request.content_type = "application/json"
-
-
-    #if @current_user.conversation_context.nil? || @current_user.conversation_context == ""
-      puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-      puts @current_user.conversation_context
-      request.body = JSON.dump({
-        "input" => {
-          "text" => params[:comment][:content]
+    request.body = JSON.dump({
+      "input" => {
+        "text" => params[:comment][:content]
+      },
+      "context" => {
+        "system" => {
+          "dialog_stack" => [
+            {
+              "dialog_node" => "root"
+            }
+          ],
+          "dialog_turn_counter" => 1,
+          "dialog_request_counter" => 1
         }
-      })
-    # else
-    #   puts "---------------------------------------------------"
-    #   puts @current_user.conversation_context
-
-    #   request.body = JSON.dump({
-    #     "input" => {
-    #       "text" => params[:comment][:content]
-    #     },
-    #     "context" => {
-
-
-    #     }
-    #   })
-    # end
+      }
+    })
 
     req_options = {
       use_ssl: uri.scheme == "https",
@@ -44,15 +35,8 @@ class CommentsController < ApplicationController
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-    puts "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
-    puts JSON.parse(response.body).to_h
-    @current_user.update(conversation_context: (JSON.parse(response.body).to_h['context']).to_s)
-    puts "==========================================================="
-    puts @current_user.conversation_context
-    puts @current_user.conversation_context.class
-
     bot_answer = JSON.parse(response.body).to_h['output']['text']
-    Comment.create! content: bot_answer.to_s[2...-2], message: @message, user: User.last
+    Comment.create! content: bot_answer[2...-2], message: @message, user: User.last
 
   end
 
