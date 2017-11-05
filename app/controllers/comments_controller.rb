22f57988913bd4b0264106d80761992a4db025d5
Sebastian Delgado <sebasdeldi@hotmail.com>
2017-11-04 19:21:20 -0500

    k

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,8 @@ class CommentsController < ApplicationController
 
 
   def create
+    puts "pppppppppppppppppppppppppppppppppppppppppppppppppp"
+    puts @current_user
     @comment = Comment.create! content: params[:comment][:content], message: @message, user: @current_user
     uri = URI.parse("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/06ffe33b-9075-411e-9b92-01ccdbe24b1f/message?version=2017-05-26")
     request = Net::HTTP::Post.new(uri)
